4f1f7a15c541f120657e5bf3d9cc8d696821ce90
John Murphy <jxm1605@student.bham.ac.uk>
2024-10-29 14:59:12 +0000

    included comments to explain referencing

--- reference_tut.cpp
+++ reference_tut.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
 void swap( double& a, double& b)
-{
+{ // the & signs declares that 'a' and 'b' are refeences 
+    // no new object is created, they simply refer to an existing object
+    //this then allows functions to act ont he ACTUAL OBJECTS passed to them rather than create a copy and do things to that.
+    // this is referred to as "passing by reference"
     double tmp {b};
     b=a;
     a=tmp;
@@ -17,4 +20,7 @@ int main()
     swap(x,y);
 
     std::cout << x << "\t" << y << "\n";
+    // in this case we first simply print x(42.3) and y(11.2)
+    // then after applying the "swap function" (utilising references) 
+    //we print them again to find the actual objects have been altered!
 }
