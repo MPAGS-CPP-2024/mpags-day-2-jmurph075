8ed313bf036aed4f3e40fcadb9e7ed80bc2f442e
John Murphy <jxm1605@student.bham.ac.uk>
2024-10-29 15:37:38 +0000

    just explained what ++ does

--- r_based_for.cpp
+++ r_based_for.cpp
@@ -6,12 +6,17 @@ int main()
 {   std::vector<int> my_ints = {1,2,3};
 
     for (const int& element : my_ints) {
+        //here we declare the elements in the vectors as constant within the for loop
     std::cout << element << "\n";
+    //so they just print as we'd expect
     }
     for (int& element : my_ints){
-    ++element;
+    ++element; // ++ just means increase by 1 increment, equivalent to +=1 in Python.
+    //here we say that we can change the values within my_ints by referencing with &
     }
     for (const int& element : my_ints){
     std::cout << element << "\n";
+    //so when we go and print them out again, they have been increments by 1
+    // i.e 2,3,4
     }
 }
